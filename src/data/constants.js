e97c8bd477815a8057c6aabfa44c112eb3865e75
Ujjwal <Ujjwal.108694@stu.upes.ac.in>
2025-01-25 22:57:45 +0530

    Files added

---
+++ src/data/constants.js
@@ -0,0 +1,378 @@
+export const Bio = {
+    name: "Ujjwal Kaushik",
+    roles: [
+      "Full Stack Developer",
+      "Android Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/ujjwal250802",
+    resume:
+      "https://drive.google.com/file/d/1sCEgymqY0GD4vps8VSknPI_kxST00bXq/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/ujjwal-kaushik-846022285/",
+    twitter: "https://twitter.com",
+    insta: "https://www.instagram.com/_ujjw.alkaushik/",
+    facebook: "https://www.facebook.com",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:
+            "https://img.icons8.com/?size=100&id=kg46nzoJrmTR&format=png&color=000000",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
+        },
+        {
+          name: "Javascript",
+          image:
+            "https://static.vecteezy.com/system/resources/previews/027/127/463/non_2x/javascript-logo-javascript-icon-transparent-free-png.png",
+        },
+        {
+          name: "Typescript",
+          image:
+            "https://cdn-icons-png.flaticon.com/256/5968/5968381.png",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-TB9d5YXwtKhv4NWbpeTBVveYvcxu9gMJng&s",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://cdn.sanity.io/images/599r6htc/regionalized/5094051dac77593d0f0978bdcbabaf79e5bb855c-1080x1080.png?w=540&h=540&q=75&fit=max&auto=format",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://ongcindia.com/documents/77751/1767704/2201_logojpg.jpg/0da1ed25-6080-dc19-7100-a5010d5dca86",
+      role: "SDE Intern",
+      company: "ONGC",
+      date: "Jan 2025 - Feb 2025",
+      desc: "Working on LLMs model,Python libraries-Gradio, Flask Creating a Chatgpt Like application with Pdf summarization feature.Utilizing Frameworks Like React Js to creating a Real-time Earthquake Monitoring application.",
+      skills: [
+        "Docker",
+        "JavaScript",
+        "Tailwind Css",
+        "Node Js",
+        "React Js",
+
+      ],
+    },
+    {
+      id: 1,
+      img: "https://www.makes360.com/presskit/images/makes360_logo.png",
+      role: "Fullstack Intern",
+      company: "Makes 360",
+      date: "June 2024 - Nov 2024",
+      desc: "Built a Clone of Company website ,  Implemented secure authentication functionalities for the website,safeguarding user data. Translated Figma design into efficient code, ensuring a seamless user experience. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "NodeJs",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "MongoDB",
+      ],
+      doc: "https://github.com/user-attachments/assets/229b01f2-4b9c-43de-8b47-87e754ece582",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://research.upes.ac.in/wp-content/uploads/2022/06/UPES-Logo-without-Tagline.jpg",
+      school: "University Of Petroleum & Energy Studies, Dehradun",
+      date: "Sep 2022 - May 2026",
+      grade: "8.45 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Univercity Of Petroleum & Energy Studies, Dehradun. I Currently in my third year in 6th semesters and have a CGPA of 8.45 . I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, Machine Learning, among others. I am also a member of the CSI  at UPES, where I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhQVheGl2AkiEKMPjZfhQiNFhguMosBZNShg&s",
+      school: "Kendriya Vidyalaya O.L.F, Dehradun",
+      date: "Apr 2019 - Apr 2020",
+      grade: "94.2%",
+      desc: "I completed my class 12 high school education at Kendriya Vidyalaya O.L.F, Dehradun, where I studied Science with Computer Science.",
+      degree: "CBSE(XII), Science with Computer",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhQVheGl2AkiEKMPjZfhQiNFhguMosBZNShg&s",
+      school: "Kendriya Vidyalaya O.L.F, Dehradun",
+      date: "Apr 2018 - Apr 2019",
+      grade: "92.8%",
+      desc: "I completed my class 10 education at Kendriya Vidyalaya O.L.F, Dehradun, where I studied Science with Computer Application.",
+      degree: "CBSE(X), Science with Computer",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Rozgar",
+      date: "Jun 2024 - Jul 2024",
+      description:
+        "Rozgar is a Job Search Platform for Blue Collar Workers with easy to understand choice-based dashboard.Implemented dashboard to display jobs and profiles with a dynamic data update.",
+      image:
+        "https://github.com/user-attachments/assets/4531d18a-1693-4824-b96b-9bd6d26c1ff5",
+      tags: [
+        "React Js",
+        "MongoDb",
+        "Node Js",
+        "Express Js",
+        "Bootstrap",
+        "HTML",
+        "CSS",
+        "NodeMailer",
+      ],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/Wageuplift-Frontend.git",
+      webapp: "https://wage-uplift-rojgaar.netlify.app/",
+    },
+    {
+      id: 0,
+      title: "ToDo Application",
+      date: "Nov 2024 - Nov 2024",
+      description:
+        "Developed a full-stack web application that allows users to perform To-Do Management: Add, edit, and delete tasks with ease, Fetch and display real-time weather data based on your location.",
+      image:
+        "https://github.com/user-attachments/assets/459e0afa-a681-4d47-8fcf-aeee968df731",
+      tags: ["React Js", "MongoDb", "Node Js", "Express Js"],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/ToDoApplicationwithAPIIntegration.git",
+      webapp: "https://meek-scone-c4fdf7.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Image Text Extractor",
+      date: "Dec 2024 - Dec 2024",
+      description:
+        "Image Text Extractor is a web application built with React.js that enables users to extract text from images, whether it's standard text or tabular data. Users can select a specific area within an image to extract the desired text efficiently.",
+      image:
+        "https://github.com/user-attachments/assets/8184091d-59ed-4e77-ae60-e4e1a60363c7",
+      tags: [
+        "React Js",
+        "Tesseract.js",
+      ],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/Image_Text_Extractor.git",
+      webapp: "https://drive.google.com/file/d/1jYrpTx554mQ39HpV4f15bDzTqU241b7P/view?usp=sharing",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation System",
+      date: "April 2024 - May 2024",
+      description:
+        "The Movie Recommendation System is a Python-based project designed to analyze movie datasets from Kaggle and provide personalized movie recommendations. The project combines data analysis and machine learning with a web-based interface for seamless user interaction.",
+      image:
+        "https://github.com/user-attachments/assets/f29f8b82-4d0e-4ebb-a7d9-b83169b80d33",
+      tags: ["Python", "Flask", "HTML", "CSS","JavaScript","Jupyter Notebook" ],
+      category: "machine learning",
+      github: "https://github.com/Ujjwal250802/movie-recommendation-system.git",
+      webapp: "https://drive.google.com/file/d/1_lGmjlNd0AlAON9cpOMphtgbEO4MAebB/view?usp=sharing",
+    },
+    {
+      id: 3,
+      title: "Quicklify",
+      date: "Dec 2024 - Jan 2025",
+      description:
+        "Developed a ChatGPT-style application for AI-driven responses and PDF summarization.Handles both text-based question answering and summarization of scanned and tabular PDFs",
+      image:
+        "https://github.com/user-attachments/assets/1b1e1bad-fd9b-4295-b9b5-d01333defbe1",
+      tags: ["Python", "Gradio", "Docker", "Ollama LLM Model","Requests","JSON"],
+      category: "machine learning",
+      github: "https://github.com/Ujjwal250802/Quicklify.git",
+      webapp: "https://drive.google.com/file/d/14NHhSH3mhos7rowOk2_NZEkJ-4954_hN/view?usp=drive_link",
+    },
+    {
+      id: 10,
+      title: "Revision-Hub",
+      date: "Aril 2024 - May-2024",
+      description:
+        "A content management platform that allows users to read published articles without login and facilitatesadmin- controlled article submissions and updates.Implemented a user approval workflow where only approved users can add, update, or delete articles through an easy-to-use word-like editor.",
+      image:
+        "https://github.com/user-attachments/assets/642e1861-19b3-442b-8d5e-e5c1530c4708",
+      tags: ["React Js", "Node Js", "MySql","Bootstrap"],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/RevisionHub.git",
+      webapp: "https://drive.google.com/file/d/16kgDnIHT9RMI-OiwhZAkOXs7DVlJubeW/view?usp=sharing",
+    },
+    {
+      id: 4,
+      title: "QuakeWatch",
+      date: "Dec 2024 - Jan 2024",
+      description:
+        "A real-time earthquake monitoring web application.Integrated live data feeds for dynamic visualization of earthquake intensity and locations.Implemented auto-refresh for continuous updates on global seismic activity.",
+      image:
+        "https://github.com/user-attachments/assets/c7e9fc3f-4c78-4b2b-88ef-a3a39e86a8f5",
+      tags: ["React Js", "Tailwind CSS","OpenLayers","REST APIs"],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/QuakeWatch.git",
+      webapp: "https://incomparable-lily-0982c7.netlify.app/",
+    },
+    {
+      id: 5,
+      title: "Sql Query Visualizer",
+      date: "Jan 2025",
+      description:
+        "Sql Query Visualizer is a web application Enables users to write and execute SQL queries seamlessly, retrieving and displaying results dynamically on the interface.  Offers an interactive visualization feature to demonstrate the step-by-step execution and functionality of SQL queries. ",
+      image:
+        "https://github.com/user-attachments/assets/b5922ecf-a46e-4fb3-80fc-2ac8ad41e88d",
+      tags: ["React Js", "Node Js", "MySql"],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/Sql_Query_Visualizer.git",
+      webapp: "https://drive.google.com/file/d/1_qoYdob_iDc9LrLTf3x-nufrgPtx7doG/view?usp=sharing",
+    },
+    {
+      id: 7,
+      title: "Social Media Handler",
+      date: "Dec 2024",
+      description:
+        "Social Media Hnadler a web application allows users to register, store credentials in MongoDB, log in, and add social media usernames and images.Features an exclusive admin interface to view all user data, including social media handles and images, with the ability to delete them.",
+      image:
+        "https://github.com/user-attachments/assets/83039b42-62d7-4771-b22a-62bbdde7bfb9",
+      tags: ["React Js", "Node Js", "Bootstrap", "MongoDB"],
+      category: "web app",
+      github: "https://github.com/Ujjwal250802/Social_media_Frontend.git",
+      webapp: "https://drive.google.com/file/d/1zTxfNbgd8Udq94-tXd4AjSa7kAIWw_Kh/view?usp=sharing",
+    },
+    {
+      id: 8,
+      title: "Weather App",
+      date: "Jan 2025",
+      description:
+        "This is a weather mobile application built with React Native and Expo.It includes Features Real-time weather data fetching using the OpenWeatherMap API,City search with autocomplete suggestions,Current weather display with temperature and conditions,8-day weather forecast,Recent searches history,Dark/Light theme toggle,Temperature unit conversion (Celsius/Fahrenheit),Smooth animations and transitions,Responsive design for various screen sizes",
+      image:
+        "https://github.com/user-attachments/assets/3230c62b-c1eb-42b0-80fd-de26c4f0e4a8",
+      tags: ["React Native", "Expo-Cli", "JavaScript/TypeScript", "Weather API"],
+      category: "android app",
+      github: "https://github.com/Ujjwal250802/WeatherApp.git",
+      webapp: "https://github.com/user-attachments/assets/965353c5-2389-4525-80ad-aa34a730efb6",
+    },
+
+  ];
+  
+  export const TimeLineData = [
+    { year: 2022, text: "Started my journey" },
+    { year: 2023, text: "Worked as a  developer" },
+    { year: 2023, text: "Founded JavaScript Mastery" },
+    { year: 2024, text: "Shared my projects with the world" },
+    { year: 2024, text: "Started my own platform" },
+  ];
+  
