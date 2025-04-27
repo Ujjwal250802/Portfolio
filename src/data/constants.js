2bdb2abb47f2544390f360234df3dd05602f715a
Ujjwal Kaushik <166251292+Ujjwal250802@users.noreply.github.com>
2025-04-27 10:41:26 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/ujjwal250802",
     resume:
-      "https://drive.google.com/file/d/1RgS7ZCAHc1I9FxueTJkwzm8WwNtjzqr7/view?usp=sharing",
+      "https://drive.google.com/file/d/1ltMdxjlMAr1z8ewt3NqSbpYxw8vAYlZx/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/ujjwal-kaushik-846022285/",
     twitter: "https://twitter.com",
     insta: "https://www.instagram.com/_ujjw.alkaushik/",
